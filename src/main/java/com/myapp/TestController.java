c6bbaab1601c5f261c27182bec12d8a7ef975b5c
tyrue <tyrue2@gmail.com>
2021-02-25 21:05:48 +0900

    스프링과 리액트 연동

---
+++ src/main/java/com/myapp/TestController.java
@@ -0,0 +1,28 @@
+package com.myapp;
+
+import java.sql.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+	
+	@PostMapping("/test")
+	public String test()
+	{
+		Date time = new Date(System.currentTimeMillis());
+		System.out.println(time);
+		return time.toString();
+	}
+	
+	@PostMapping("/ip")
+	public ResponseEntity<String> ip (HttpServletRequest request) {
+		// 요청을 보낸 클라이언트의 IP주소를 반환합니다.
+		return ResponseEntity.ok(request.getRemoteAddr());
+	}
+}
