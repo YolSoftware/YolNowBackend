b35b450d363e2eab0538ab7f3c5913561cc3ab00
Seongsik Kim <kss7858@naver.com>
2021-02-25 21:35:30 +0900

    add: initial set proejct "Hello World"
    Signed-off-by: Seongsik Kim <kss7858@naver.com>

---
+++ yolchat/src/test/java/com/chat/yolchat/YolchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.yolchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YolchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
