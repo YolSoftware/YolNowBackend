32ec667c2e7c66270bc5c4ec707dd82e6b15de95
tyrue <kdh9426@naver.com>
2021-04-01 19:50:32 +0900

    수정 : 주석 지움

--- yolchat/src/main/java/com/chat/yolchat/YolchatApplication.java
+++ yolchat/src/main/java/com/chat/yolchat/YolchatApplication.java
@@ -9,11 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @RestController
 @SpringBootApplication
-// 하위에 있는걸 스프링이 component 스캔한다
-// 그다음 등록한다.
-// 싹다
-// 하지만 Reposity는 component 하위에 있어서 component대상이기 때문에
-// Reposityory도 component 스캔이 되어 등록
 public class YolchatApplication {
 
     @RequestMapping("/")
